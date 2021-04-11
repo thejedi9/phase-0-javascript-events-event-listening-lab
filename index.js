7e9f977eb9d25d09c35e1d95df1242f9ff47f65b
Jede <brahmajede@gmail.com>
2021-04-11 15:46:11 -0400

    lab done

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+console.log("Jede's JS console check!")
 function addingEventListener() {
+
+const input = document.getElementById('input');
+    function clickAlert() {
+        alert('I was clicked!');
+    } 
+input.addEventListener('click', clickAlert);
+
+console.log(this.input)
+  
 }
+addingEventListener()
+
+
+
+    
+    
+
+ 
+
+
+    
